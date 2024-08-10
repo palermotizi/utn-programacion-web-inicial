7b1cfc63e5e4ff4e13500ba9097a7d39d110b400
Palermo Tizi <palermotiziana1@gmail.com>
2024-08-10 16:12:28 -0300

    step 1

--- whatsapp-clase-23/src/Context/GlobalContext.jsx
+++ whatsapp-clase-23/src/Context/GlobalContext.jsx
@@ -51,8 +51,7 @@ export const GlobalProvider = ({ children }) => {
   )
 }
 
-export const useGlobalContext = () => {
-  return useContext(GlobalContext)
+export const useGlobalContext = () => 
 }
 
 
